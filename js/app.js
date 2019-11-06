84f5ff54d448e068c01a52d8eea4916d8f3f6c4b
Atle Lohrmann <54595279+AtleLohrmann@users.noreply.github.com>
2019-11-06 06:56:04 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,47 +1,71 @@
-// grab element with id of #roll-dice and listen
-// for click event
-// when click event occurs "call" the rollDice function
+// Js to handle changes to the battery planning tool
+// The first part is to initate the values since we want to be absolute
+// able to calculate the result as the user changes one and one parameter
+// Should ideally be coupled to the index file
+
+let period =4
+let interval=10
 
 
-$(function () {
 
-document.getElementById('roll-dice').onclick = rollDice
+$(function() {
 
-$('roll-dice').click(rollDice)
 
-function rollDice () {
-  // randomly generate number from 1 to 6 and store
-  // values in variables (randomDie1 & randomDie2)
-  const randomDie1 = generateRandomDie()
-  const randomDie2 = generateRandomDie()
+    $('#submit-btn2').click(() => {
+			period = $('#period').val()
+			showConsumption(period,interval)
+    })
 
-  // dynamically generate class names
-  // that correspond to the random numbers
-  const firstDieClass = `dice dice-${randomDie1}`
-  const secondDieClass = `dice dice-${randomDie2}`
+		$('#submit-btn3').click(() => {
+			interval = $('#interval').val()
+			showConsumption(period,interval)
 
-  console.log(`randomDie1: ${randomDie1}`)
-  console.log(`randomDie2: ${randomDie2}`)
+		})
 
-  console.log(`firstDieClass: ${firstDieClass}`)
-  console.log(`secondDieClass: ${secondDieClass}`)
 
-  updatePage(firstDieClass, secondDieClass)
-}
+		function showConsumption(period,interval) {
+			consume = quickCalc(period, interval)
+			console.log(consume)
+			battery(consume)
+		}
 
-// specify function only responsible for
-// generating random numbers
-function generateRandomDie () {
-  return Math.floor(Math.random() * 6) + 1
-}
+		// Here we should add an reset button if we have time
 
-// specify function only responsible for
-// updating our page
-function updatePage (firstClass, secondClass) {
-  // dynamically apply new class names to the dom (html page)
-  //document.getElementById('first-die').className = firstClass
-  //document.getElementById('second-die').className = secondClass
-$("#first-die").attr('class',firstClass)
-$("#second-die").attr('class',secondClass)
-}
-} //end of document ready function
+
+		// Calculate the batttery consumption - based on a simple,
+		// 20 days/10 Minutes calculation
+		function quickCalc(days,mInt) {
+
+			const totSamples=20*24*60/10  //Total number of sample
+			const desiredSamples=days*24*60/mInt		// The planned number totSamples
+			batteryConsumption = desiredSamples/totSamples
+			console.log(batteryConsumption)
+			return batteryConsumption
+		}
+
+
+		function battery (percentage) {
+			// Here we want to draw the updated % used of the battery into the blue boxtext
+			// and we want to indicate how much battery is left by drawing up the battery
+			// Pick out the image number to uss:
+			let imageNumber=Math.round((1-percentage)*100/20)
+			// Generate the image number
+			let imageName=`image/ECO-battery-${imageNumber}.png`
+			if (percentage>=1) {
+				imageName="image/ECO-battery-red.png"
+			}
+			//Send the new image to the screen, id=battery
+			document.getElementById('battery').src=imageName;
+			// convert the percentage to a whole number and add the % sign
+			let percTxt=`${Math.round(percentage*100)}%`
+			// change the text in the blue box on the screen
+			document.getElementById("result").innerHTML = percTxt
+
+			console.log(percentage)
+			console.log(imageName)
+			console.log(imageNumber)
+			console.log(percTxt)
+
+			return
+		}
+})
